67fd616e88f806a70c220b35918c7679b661a0f0
Ryan Namgung <heon.namgung@richmond.edu>
2018-05-23 04:41:56 +0000

    automatically backed up by learn

--- js/events.js
+++ js/events.js
@@ -1,7 +1,25 @@
 //define functions here
+function getIt()
+{
+  
+}
+
+function frameIt()
+{
+  
+}
+
+function pressIt()
+{
+  
+}
+
+function submitIt()
+{
+  
+}
 
 $(document).ready(function(){
-
 // call functions here
 
 });
