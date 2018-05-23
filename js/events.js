1a5df6b7ff77a548e258d1f2a6d58b75a58958c2
Ryan Namgung <heon.namgung@richmond.edu>
2018-05-23 04:50:57 +0000

    automatically backed up by learn

--- js/events.js
+++ js/events.js
@@ -24,7 +24,7 @@ function pressIt()
 
 function submitIt()
 {
-  $(form).on('submit', function(){
+  $('form').on('submit', function(){
     alert("Your form is going to be submitted now.")
   })
 }
